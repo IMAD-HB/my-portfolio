fc9a0c379d3919a7f238f20a000fae0b3cf40fb6
IMAD-HB <habbiimadeddine@gmail.com>
2025-05-25 14:35:04 +0100

    Added 2 projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -120,6 +120,67 @@ const Projects = () => {
             </div>
 
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_#22c55e1a] transition">
+              <h3 className="text-xl font-bold mb-2">
+                Tesla Landing Page Clone
+              </h3>
+              <p className="text-gray-400 mb-4">
+                A modern, responsive clone of the Tesla homepage. Highlights
+                include clean design, smooth animations, and a polished layout
+                closely matching the original.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "TailwindCSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_#22c55e33] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://tesla-landing-page-steel.vercel.app"
+                  target="_blank"
+                  className="text-green-400 hover:text-green-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_#22c55e1a] transition">
+              <h3 className="text-xl font-bold mb-2">Chef Claude</h3>
+              <p className="text-gray-400 mb-4">
+                An AI-powered recipe generator that creates custom recipes based
+                on user-provided ingredients. Designed to offer quick, creative
+                meal ideas using Claude AI, with a clean and intuitive interface
+                for easy input and output.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "TailwindCSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_#22c55e33] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://chef-claude-6z0x963qm-habbi-imad-eddines-projects.vercel.app"
+                  target="_blank"
+                  className="text-green-400 hover:text-green-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_#22c55e1a] transition">
               <h3 className="text-xl font-bold mb-2">Tenzies Game</h3>
               <p className="text-gray-400 mb-4">
                 A simple yet engaging dice game where players roll and hold dice
